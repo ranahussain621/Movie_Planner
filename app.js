84b4f42c692649851e3e369c9023b0d8653aea0a
Rana Hussain Sakhawat <ranahussain621@gmail.com>
2021-12-25 20:59:20 +0500

    index.html and app.js file changed
    Both index.html and app.js file changed acording to requirements

--- app.js
+++ app.js
@@ -15,36 +15,46 @@ function main() {
 
         });
 
-
-        ////add movie
-
-// const addForm = forms['add-movie'];
-// addForm.addEventListener('submit',function(e){
-// e.preventDefault();
-// })
-
-// ////create elements
-
-// const value = addForm.querySelector('input[type="text"]').value;
-// const li = document.createElement('li');
-// const movieName = document.createElement('span');
-// const deleteBtn = document.createElement('span');
-// const inputtt = document.getElementById('lastInput')
-
-// ///// add text content
-
-// movieName.textContent = inputtt.value;
-// deleteBtn.textContent = "delete";
-
-// //// add classes 
-// movieName.classList.add('name');
-// deleteBtn.classList.add('delete');
-
-// //// append your content
-
-// li.appendChild(movieName);
-// li.appendChild(deleteBtn);
-// list.appendChild(li);
-
-
 }
+        ////add movie
+        
+
+        function addMovie(){
+
+            const addForm = document.getElementById('add-movie')
+            addForm.addEventListener('submit',function(e){
+            e.preventDefault();
+            })
+            
+            ////create elements
+            
+            const value = addForm.querySelector('input[type="text"]').value;
+            const li = document.createElement('li');
+            const movieName = document.createElement('span');
+            const deleteBtn = document.createElement('span');
+            const inputtt = document.getElementById('lastInput')
+            
+            ///// add text content
+            
+            movieName.textContent = inputtt.value;
+            deleteBtn.textContent = "delete";
+            
+            //// add classes 
+            movieName.classList.add('name');
+            deleteBtn.classList.add('delete');
+            
+            //// append your content
+            
+            li.appendChild(movieName);
+            li.appendChild(deleteBtn);
+            list.appendChild(li);
+
+            var input = document.getElementById('lastInput');
+            input.focus();
+            input.value = ' ';
+
+        }
+
+
+
+
